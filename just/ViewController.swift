212669b2246637fbe4531f63779eee149bc23786
MaurZac <maur.mx@gmail.com>
2022-08-21 09:20:48 -0500

    Initial Commit

---
+++ just/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  just
+//
+//  Created by Mauricio Zarate Chula on 21/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
